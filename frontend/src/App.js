33036b6a11cfaffda180bdfd4ae14d2eb20e5b97
MissPhantom <missphantom999@gmail.com>
2025-11-15 13:22:18 +0100

    Add frontend project

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div style={{ padding: "40px",color:'#182346' , backgroundColor: '#3ed9d9', fontFamily: 'Grotesque' }}>
+      <h1>EMSS Blog</h1>
+      <h3>Welcome to Engineering Muslim Students Society Blog</h3>
+      <p>This is the frontend of my Capstone Project.</p>
+    </div>
+  );
+}
+
+export default App;
