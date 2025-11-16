a25548749c48c7069f0bde69080445d8ab914fca
MissPhantom <missphantom999@gmail.com>
2025-11-16 20:09:00 +0100

    Added the tf files and custom VPC

---
+++ terraform/vpc.tf
@@ -0,0 +1,39 @@
+# ---------------------------
+# Custom VPC
+# ---------------------------
+resource "aws_vpc" "main" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "capstone-vpc"
+  }
+}
+
+# ---------------------------
+# Public Subnet 1
+# ---------------------------
+resource "aws_subnet" "public_1" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+  availability_zone       = "us-east-1a"
+
+  tags = {
+    Name = "public-subnet-1"
+  }
+}
+
+# ---------------------------
+# Private Subnet 1
+# ---------------------------
+resource "aws_subnet" "private_1" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "us-east-1a"
+
+  tags = {
+    Name = "private-subnet-1"
+  }
+}
