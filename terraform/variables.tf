b76a29b3620c19ba821f5640d25cfe2c0e9644e8
MissPhantom <missphantom999@gmail.com>
2025-11-17 10:44:48 +0100

    Added terraform files

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,11 +1,29 @@
-variable "aws_region" {
-  description = "AWS region to deploy resources"
+variable "project_name" {
+  description = "Name of the project"
   type        = string
-  default     = "us-east-1"
+  default     = "capstone"
 }
 
-variable "instance_type" {
-  description = "EC2 instance type"
+variable "region" {
+  description = "AWS region"
   type        = string
-  default     = "t2.micro"
+  default     = "eu-west-1"
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "public_subnet_cidrs" {
+  description = "List of public subnet CIDR blocks"
+  type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24"]
+}
+
+variable "private_subnet_cidrs" {
+  description = "List of private subnet CIDR blocks"
+  type        = list(string)
+  default     = ["10.0.3.0/24", "10.0.4.0/24"]
 }
